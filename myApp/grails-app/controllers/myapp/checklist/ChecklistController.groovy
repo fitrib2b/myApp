e15419a7afe8e4eed90750f277fc69e298fe0b09
fitrib2b <fitri@b2b.com.my>
2022-04-12 11:04:03 +0800

    Checklist domain and controllers + the user webpages

---
+++ myApp/grails-app/controllers/myapp/checklist/ChecklistController.groovy
@@ -0,0 +1,6 @@
+package myapp.checklist
+
+class ChecklistController {
+
+    def index() { }
+}
--- myApp/grails-app/domain/myapp/Checklist.groovy
+++ myApp/grails-app/domain/myapp/Checklist.groovy
@@ -2,6 +2,18 @@ package myapp
 
 class Checklist {
 
+    String id
+    String taskName
+    boolean complete
+    Date dateCreated
+    Date dateCompleted
+
     static constraints = {
+        id column: 'id', generator: 'uuid', type: 'string'
+        taskName maxSize: 255, nullable: false
+        complete inList: ['y','n'], nullable: false
+        dateCreated date: true, nullable: false, validator: {val -> validate(val)}
+        dateCompleted date: true, nullable: false, validator: {val ->validate(val)}
     }
+
 }
